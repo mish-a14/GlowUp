48da7e78956bb1421ce07a9001e0e87f98cbe94a
Nicole WItherall <nicwitherall@gmail.com>
2021-04-07 10:58:19 -0400

    hello

--- main_app/urls.py
+++ main_app/urls.py
@@ -9,42 +9,48 @@ urlpatterns = [
     path('accounts/signup/', views.signup, name='signup'),
 
 
-    #this is the user's log show page
-    path('log/', views.log),
+    # this is the user's log show page
+    path('log/hair/', views.hair_log),
+    path('log/skin/', views.skin_log),
 
-    #these are the paths to get to the forms for the user to enter thier diary input
+    # these are the paths to get to the forms for the user to enter thier diary input
     path('log/hairdiary/', views.hairdiary),
-    path('log/skindiary/', views.skindiary), 
+    path('log/skindiary/', views.skindiary),
 
-    #this is the user's log' detail page of every log 
+    # this is the user's log' detail page of every log
     path('log/<int:hair_id>/', views.hair_detail),
 
-    #path for adding a log for either hair or skin form
-    path('log/addlog/', views.add_log),
+    # path for adding a log for either hair or skin form
 
-    #create forms for hair diary
+    # create forms for hair diary
     path('log/hairdiary/create_form/', views.create_form),
     path('log/hairdiary/submit_create_form/', views.submit_create_form),
-   
-    #delete hair diary
+
+    # delete hair diary
     path('log/<int:h_id>/delete/', views.delete),
 
-    #update hair diary step 1: deliver form to user
+    # update hair diary step 1: deliver form to user
     path('log/<int:h_id>/edit/', views.edit_form),
-    #update hair diary step 2: accept form from user
+    # update hair diary step 2: accept form from user
     path('log/<int:h_id>/hairdiary/submit_update_form/', views.submit_update_form),
 
 
-    #create forms for skin diary
-    path('log/skindairy/skin_create_form/', views.skin_create_form),
+    # create forms for skin diary
+    path('log/skindiary/create_form/', views.skin_create_form),
     path('log/skindiary/submit_create_form/', views.submit_skin_form),
-    
-    #create skin diary 
-    #update skin diary 
-    #delete skin diary
 
-    #PHOTOS 
-   # path('log/<int:hair_id>/add_photo/', views.add_photo, name='add_photo'),
-     
+    # this is the user's log' detail page of every log
+    path('log/<int:skin_id>/', views.skin_detail),
+
+    # create skin diary
+    # update skin diary
+    path('log/<int:s_id>/edit/', views.skin_edit_form),
+    # update hair diary step 2: accept form from user
+    path('log/<int:s_id>/hairdiary/submit_update_form/',
+         views.skin_submit_update_form),
+    # delete skin diary
+
+    # PHOTOS
+    # path('log/<int:hair_id>/add_photo/', views.add_photo, name='add_photo'),
+
 ]
-
--- main_app/views.py
+++ main_app/views.py
@@ -13,135 +13,190 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 
 # Define the home view
+
+
 def home(request):
-  return render(request, 'home.html')
+    return render(request, 'home.html')
+
 
 def supplements(request):
-  return render(request, 'about.html')
+    return render(request, 'about.html')
+
 
 def products(request):
-  return render(request, 'about.html')
+    return render(request, 'about.html')
+
 
 def plan(request):
-  return render(request, 'about.html')
-
+    return render(request, 'about.html')
 
 
 @login_required
-def log(request):
-  hair = HairDiary.objects.all()
-  hair = HairDiary.objects.filter(user = request.user)
-  #hair = request.user.hair_set.all()
-  return render(request, 'log.html', { 'hair': hair })
+def hair_log(request):
+    hair = HairDiary.objects.filter(user=request.user)
+    return render(request, 'hair_log.html', {'hair': hair})
+
+
+@login_required
+def skin_log(request):
+    skin = SkinDiary.objects.filter(user=request.user)
+    return render(request, 'skin_log.html', {'skin': skin})
 
 
 @login_required
 def hair_detail(request, hair_id):
-  hair = HairDiary.objects.get(id=hair_id)
-  print(hair)
-  print(request)
-  return render(request, 'hair_log_detail.html', { 'hair': hair})
+    hair = HairDiary.objects.get(id=hair_id)
+    print(hair)
+    print(request)
+    return render(request, 'log_detail.html', {'hair': hair})
 
 
+@login_required
+def skin_detail(request, skin_id):
+    skin = SkinDiary.objects.get(id=skin_id)
+    print(request)
+    print(skin)
+    return render(request, 'skin_log_detail.html', {'skin': skin})
 
 
 def hairdiary(request):
-  return render(request, 'diary.html')
+    return render(request, 'diary.html')
+
 
 def skindiary(request):
-  return render(request, 'diary.html')
+    return render(request, 'diary.html')
 
 
-#page to add log that displays both HairDiary and Skin Diary buttons
-def add_log(request):
-  return render(request, 'add_log.html')
+# page to add log that displays both HairDiary and Skin Diary buttons
+def add_hair_log(request):
+    return render(request, 'create_form.html')
 
 
-#HAIR DIARY FORM 
-#@login_required
+def add_skin_log(request):
+    return render(request, 'create_form.html')
+
+
+# HAIR DIARY FORM
+@login_required
 def create_form(request):
-  return render(request, 'create_form.html')
+    return render(request, 'create_form.html')
 
-#hair diary create
+# hair diary create
+
+
 @login_required
 def submit_create_form(request):
-  HairDiary.objects.create(
-    Log=request.POST['log'],
-    Date=request.POST['date'],
-    user=request.user,
-  )
-  return redirect('/log')
+    HairDiary.objects.create(
+        Log=request.POST['log'],
+        Date=request.POST['date'],
+        Water=request.POST['water'],
+        Product=request.POST['product'],
+        Morning=request.POST['morning'],
+        Night=request.POST['night'],
+        user=request.user,
+    )
+    return redirect('/log/hair/')
 
 
-#hair diary delete 
+# hair diary delete
 def delete(request, h_id):
-  h = HairDiary.objects.get(id=h_id)
-  h.delete()
-  return redirect('/log')
+    h = HairDiary.objects.get(id=h_id)
+    h.delete()
+    return redirect('/log/hair/')
 
-#hair diary edit 
+# hair diary edit
+
+
 def edit_form(request, h_id):
-  #get the particular hair post i'm editing by id
-  h = HairDiary.objects.get(id=h_id)
-  return render(request, 'edit_form.html', {'h': h })
+    # get the particular hair post i'm editing by id
+    h = HairDiary.objects.get(id=h_id)
+    return render(request, 'edit_form.html', {'h': h})
 
-#hair diary submit of update form after user has made edit 
+# hair diary submit of update form after user has made edit
+
+
+@login_required
 def submit_update_form(request, h_id):
-  this_entry = HairDiary.objects.get(id=h_id)
-  this_entry.Log = request.POST['log']
-  this_entry.Date = request.POST['date']
-  this_entry.save()
-  print(this_entry)
-  return redirect('/log')
- 
+    this_entry = HairDiary.objects.get(id=h_id)
+    this_entry.Log = request.POST['log']
+    this_entry.Date = request.POST['date']
+    this_entry.Water = request.POST['water']
+    this_entry.Product = request.POST['product']
+    this_entry.Morning = request.POST['morning']
+    this_entry.Night = request.POST['night']
+    this_entry.save()
+    print(this_entry)
+    return redirect('/log/hair/')
 
 
-
-#skin Diary FORM 
+# skin Diary FORM
 def skin_create_form(request):
-  return render(request, 'skin_create_form.html')
+    return render(request, 'skin_create_form.html')
 
-#skin diary create
+# skin diary create
+
+
 @login_required
 def submit_skin_form(request):
-  SkinDiary.objects.create(
-    Log=request.POST['log'],
-    Date=request.POST['date'],
-    user=request.user,
-  )
-  return redirect('/log')
+    SkinDiary.objects.create(
+        Log=request.POST['log'],
+        Date=request.POST['date'],
+        Water=request.POST['water'],
+        Product=request.POST['product'],
+        Morning=request.POST['morning'],
+        Night=request.POST['night'],
+        user=request.user,
+    )
+    return redirect('/log/skin/')
 
 
+# skin diary update
 
-#skin diary update
-#skin diary delete 
-#note: i have already imported both of the models at the top! 
-
-
-#PHOTOS STUFF
-
-#def add_photo(request):
+def skin_edit_form(request, s_id):
+    # get the particular hair post i'm editing by id
+    s = SkinDiary.objects.get(id=s_id)
+    return render(request, 'skin_edit_form.html', {'s': s})
 
 
+@login_required
+def skin_submit_update_form(request, s_id):
+    this_entry = SkinDiary.objects.get(id=s_id)
+    this_entry.Log = request.POST['log']
+    this_entry.Date = request.POST['date']
+    this_entry.Water = request.POST['water']
+    this_entry.Product = request.POST['product']
+    this_entry.Morning = request.POST['morning']
+    this_entry.Night = request.POST['night']
+    this_entry.save()
+    print(this_entry)
+    return redirect('/log/skin/')
+# skin diary delete
+# note: i have already imported both of the models at the top!
 
-#LOG IN STUFF
+
+# PHOTOS STUFF
+
+# def add_photo(request):
+
+
+# LOG IN STUFF
 
 
 def form_valid(self, form):
-  form.instance.user = self.request.user 
-  return super().form_valid(form)
+    form.instance.user = self.request.user
+    return super().form_valid(form)
 
 
 def signup(request):
-  error_message = ''
-  if request.method == 'POST':
-    form = UserCreationForm(request.POST)
-    if form.is_valid():
-      user = form.save()
-      login(request, user)
-      return redirect('home')
-    else:
-      error_message = 'Invalid sign up - try again'
-  form = UserCreationForm()
-  context = {'form': form, 'error_message': error_message}
-  return render(request, 'registration/signup.html', context)
+    error_message = ''
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            error_message = 'Invalid sign up - try again'
+    form = UserCreationForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'registration/signup.html', context)
